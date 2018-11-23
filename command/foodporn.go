9cd7d8fe6b30ecfa749cb78280e1ccf7970f4e0e
makitune <msk.kawano@gmail.com>
2018-11-23 21:22:07 +0900

    HeadsUpコマンドをファイルごと分離

--- command/foodporn.go
+++ command/foodporn.go
@@ -3,7 +3,6 @@ package command
 import (
 	"math/rand"
 	"strings"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/makitune/discob/errr"
@@ -54,50 +53,4 @@ func (bot *Bot) FoodPorn(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		}
 	}
-}
-
-func (bot *Bot) HeadsUp(s *discordgo.Session, p *discordgo.PresenceUpdate) {
-	if string(p.Status) != "online" {
-		return
-	}
-
-	go func() {
-		u := p.User
-		g, err := s.Guild(p.GuildID)
-		if err != nil {
-			errr.Printf("%s\n", err)
-			return
-		}
-
-		var c *discordgo.Channel
-		for _, ch := range g.Channels {
-			if ch.Type == 0 && ch.Position == 0 {
-				c = ch
-			}
-		}
-
-		if c == nil {
-			return
-		}
-
-		t := time.NewTicker(1 * time.Hour)
-		for {
-			<-t.C
-			for _, p := range g.Presences {
-				id := p.User.ID
-				if id == u.ID {
-					if string(p.Status) != "online" {
-						t.Stop()
-						return
-					}
-
-					msg := u.Mention() + "、あなた疲れてるのよ\n"
-					sendMessage(s, c, msg)
-
-					max := len(fpkws)
-					bot.sendImage(s, c, fpkws[rand.Intn(max)])
-				}
-			}
-		}
-	}()
 }
