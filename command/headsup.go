73990ae8465ca9b74dc3f9a41bb0a471a623787f
makitune <msk.kawano@gmail.com>
2018-11-24 12:54:37 +0900

    HeadsUpのgoroutine部分を関数に分離

--- command/headsup.go
+++ command/headsup.go
@@ -13,47 +13,49 @@ func (bot *Bot) HeadsUp(s *discordgo.Session, p *discordgo.PresenceUpdate) {
 		return
 	}
 
-	go func() {
-		u := p.User
-		g, err := s.Guild(p.GuildID)
-		if err != nil {
-			errr.Printf("%s\n", err)
-			return
+	go headsup(s, p, bot)
+}
+
+func headsup(s *discordgo.Session, p *discordgo.PresenceUpdate, b *Bot) {
+	u := p.User
+	g, err := s.Guild(p.GuildID)
+	if err != nil {
+		errr.Printf("%s\n", err)
+		return
+	}
+
+	var c *discordgo.Channel
+	for _, ch := range g.Channels {
+		if ch.Type == 0 && ch.Position == 0 {
+			c = ch
 		}
+	}
 
-		var c *discordgo.Channel
-		for _, ch := range g.Channels {
-			if ch.Type == 0 && ch.Position == 0 {
-				c = ch
-			}
-		}
+	if c == nil {
+		return
+	}
 
-		if c == nil {
-			return
-		}
-
-		t := time.NewTicker(1 * time.Hour)
-		for {
-			<-t.C
-			for _, p := range g.Presences {
-				id := p.User.ID
-				if id == u.ID {
-					if string(p.Status) != "online" {
-						t.Stop()
-						return
-					}
-
-					msg := u.Mention()
-					if m, err := bot.headsUpMessage(); err == nil {
-						msg = msg + "\t" + m
-					}
-
-					sendMessage(s, c, msg)
-
-					max := len(fpkws)
-					bot.sendImage(s, c, fpkws[rand.Intn(max)])
+	t := time.NewTicker(1 * time.Hour)
+	for {
+		<-t.C
+		for _, p := range g.Presences {
+			id := p.User.ID
+			if id == u.ID {
+				if string(p.Status) != "online" {
+					t.Stop()
+					return
 				}
+
+				msg := u.Mention()
+				if m, err := b.headsUpMessage(); err == nil {
+					msg = msg + "\t" + m
+				}
+
+				sendMessage(s, c, msg)
+
+				max := len(fpkws)
+				b.sendImage(s, c, fpkws[rand.Intn(max)])
 			}
 		}
-	}()
+	}
 }
