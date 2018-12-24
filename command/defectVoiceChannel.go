81af534d20c8dbcb234f03430ed63e816c12f2ab
makitune <msk.kawano@gmail.com>
2018-12-24 16:24:46 +0900

    VoiceChannelに参加していない場合は早期リターン

--- command/defectVoiceChannel.go
+++ command/defectVoiceChannel.go
@@ -11,6 +11,10 @@ var (
 )
 
 func (bot *Bot) DefectVoiceChannel(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if bot.voiceConnection == nil {
+		return
+	}
+
 	if !strings.Contains(m.Content, defectTrigger) {
 		return
 	}
