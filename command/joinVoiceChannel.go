fd2efb55caecb0c9d5e116d65cebe21e04f1987d
makitune <msk.kawano@gmail.com>
2018-12-24 15:40:35 +0900

    VoiceChannelを取得

--- command/joinVoiceChannel.go
+++ command/joinVoiceChannel.go
@@ -1,7 +1,10 @@
 package command
 
 import (
+	"errors"
 	"strings"
+
+	"github.com/makitune/discob/errr"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -18,4 +21,32 @@ func (bot *Bot) JoinVoiceChannel(s *discordgo.Session, m *discordgo.MessageCreat
 	if m.Author.Username == bot.config.Discord.UserName || m.Author.Bot {
 		return
 	}
+
+	c, err := s.Channel(m.ChannelID)
+	if err != nil {
+		errr.Printf("%s\n", err)
+		return
+	}
+
+	vChan, err := findVoiceChannel(s, c.GuildID)
+	if err != nil {
+		errr.Printf("%s\n", err)
+		bot.sendErrorMessage(s, c, err)
+		return
+	}
+}
+
+func findVoiceChannel(s *discordgo.Session, guildID string) (*discordgo.Channel, error) {
+	g, err := s.Guild(guildID)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range g.Channels {
+		if c.Type == discordgo.ChannelTypeGuildVoice {
+			return c, nil
+		}
+	}
+
+	return nil, errors.New("VoiceChannel not found")
 }
