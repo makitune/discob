830d17ed04786740e790162694da941b24dd33a5
makitune <msk.kawano@gmail.com>
2018-12-24 19:21:39 +0900

    音楽停止コマンドを作成

---
+++ command/stopMusic.go
@@ -0,0 +1,28 @@
+package command
+
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	stopTrigger = "うるさいですね"
+)
+
+func (bot *Bot) StopMusic(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if bot.stopChan == nil {
+		return
+	}
+
+	if m.Author.Username == bot.config.Discord.UserName || m.Author.Bot {
+		return
+	}
+
+	if !strings.Contains(m.Content, stopTrigger) {
+		return
+	}
+
+	*bot.stopChan <- true
+	bot.stopChan = nil
+}
