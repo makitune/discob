77534a78f4ecbe6f8a8b034404bd4743ecc89c1b
makitune <msk.kawano@gmail.com>
2019-01-27 19:42:25 +0900

    メッセージのユーザが自分もしくはボットなら離脱

--- command/wikipedia.go
+++ command/wikipedia.go
@@ -10,4 +10,9 @@ func (bot *Bot) Wikipedia(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if !strings.HasSuffix(m.Content, "ってしってる？") {
 		return
 	}
+
+	user := m.Author
+	if user.Username == bot.config.Discord.UserName || user.Bot {
+		return
+	}
 }
