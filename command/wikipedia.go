b82fcb29f87396cd00720231969b2cd96db4ad60
makitune <msk.kawano@gmail.com>
2019-01-27 19:41:16 +0900

    “ってしってる？”で発火

--- command/wikipedia.go
+++ command/wikipedia.go
@@ -1,6 +1,13 @@
 package command
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func (bot *Bot) Wikipedia(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if !strings.HasSuffix(m.Content, "ってしってる？") {
+		return
+	}
 }
