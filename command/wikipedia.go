fd1f253bf878c4d08a0729808c7623ada345bcbc
makitune <msk.kawano@gmail.com>
2019-01-27 20:06:19 +0900

    WikipediaのURLを取得

--- command/wikipedia.go
+++ command/wikipedia.go
@@ -3,6 +3,8 @@ package command
 import (
 	"strings"
 
+	"github.com/makitune/discob/command/search"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/makitune/discob/errr"
 )
@@ -17,7 +19,7 @@ func (bot *Bot) Wikipedia(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	_, err := s.State.Channel(m.ChannelID)
+	c, err := s.State.Channel(m.ChannelID)
 	if err != nil {
 		errr.Printf("%s\n", err)
 		return
@@ -25,4 +27,9 @@ func (bot *Bot) Wikipedia(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	i := strings.Index(m.Content, "ってしってる？")
 	keyword := m.Content[:i]
+	urlString, err := search.SearchWikipediaURL(keyword)
+	if err != nil {
+		bot.sendErrorMessage(s, c, err)
+		return
+	}
 }
